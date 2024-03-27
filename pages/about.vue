f4c7a66874e2f517ff5d63f49f94ae42b80e9e8a
ndaden <nabil.daden@gmail.com>
2024-03-28 00:16:54 +0100

    first

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">NUXT Dojo</NuxtLink>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                        <NuxtLink to="/about">About</NuxtLink>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vitae aliquam assumenda eaque rem tenetur
+            molestias necessitatibus. Facere, nobis minima.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vitae aliquam assumenda eaque rem tenetur
+            molestias necessitatibus. Facere, nobis minima.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+<p>Product details for {{ id }}</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quod hic quisquam officiis doloribus! Odit ea ab quisquam beatae eligendi ex, unde cupiditate ullam iste.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
